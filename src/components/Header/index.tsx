a59a90a7d325d68fc6b40e2c8f2b130906477722
rayjackson13 <rayjackson@live.ru>
2022-02-02 22:09:43 +0600

    Added header and Sass functions.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,83 +1,18 @@
 import React from 'react';
 
-export const Header = (): JSX.Element => (
-  <aside className="header-follow">
-    <div className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <span className="accent">Ray</span> 7ackson
-        </a>
-        <button
-          aria-controls="navbarsExampleDefault"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          className="navbar-toggler"
-          data-target="#navbarsExampleDefault"
-          data-toggle="collapse"
-          type="button"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+import * as styles from './Header.module.scss';
 
-        <nav className="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul className="social ml-auto">
-            <li className="social-link">
-              <a
-                className="social-link_item"
-                data-link="bandcamp"
-                href="https://rayjackson.bandcamp.com/"
-                rel="nofollow noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-bandcamp" />
-              </a>
-            </li>
-            <li className="social-link">
-              <a
-                className="social-link_item"
-                data-link="soundcloud"
-                href="https://soundcloud.com/rayjackson13"
-                rel="nofollow noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-soundcloud" />
-              </a>
-            </li>
-            <li className="social-link">
-              <a
-                className="social-link_item"
-                data-link="vk"
-                href="https://vk.com/jakeminor"
-                rel="nofollow noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-vk" />
-              </a>
-            </li>
-            <li className="social-link">
-              <a
-                className="social-link_item"
-                data-link="twitter"
-                href="https://twitter.com/RayJackson132"
-                rel="nofollow noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-twitter" />
-              </a>
-            </li>
-            <li className="social-link">
-              <a
-                className="social-link_item"
-                data-link="instagram"
-                href="https://www.instagram.com/rayjackson132"
-                rel="nofollow noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-instagram" />
-              </a>
-            </li>
-          </ul>
-        </nav>
+import { Socials } from 'components/Socials';
+
+export const Header = (): JSX.Element => (
+  <aside className={styles.header}>
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        <a className={styles.brand} href="/">
+          <span className={styles.accent}>Ray</span> 7ackson
+        </a>
+
+        <Socials classes={styles.socials} />
       </div>
     </div>
   </aside>
--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 import * as styles from './Socials.module.scss';
 import socialInfo from './social.json';
 
-export const Socials = (): JSX.Element => (
-  <div>
+type Props = {
+  classes?: string;
+};
+
+export const Socials = ({ classes }: Props): JSX.Element => (
+  <div className={classes}>
     <ul className={styles.social}>
       {socialInfo.streamingPlatforms.map((val, idx) => (
         <li key={idx}>
